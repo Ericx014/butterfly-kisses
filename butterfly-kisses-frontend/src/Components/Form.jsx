f327807761ec66129b875ae1a1792e51598c46a6
Eric Bong <ericbong6868@gmail.com>
2024-04-16 19:03:46 +0800

    Add index2.css updated styling

--- butterfly-kisses-frontend/src/Components/Form.jsx
+++ butterfly-kisses-frontend/src/Components/Form.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Form = () => {
   const [remarks, setRemarks] = useState("");
 	const [clickedAnimation, setClickedAnimation] = useState(false);
+	const [value, setValue] = useState("");
 
   const handleRemarksChange = (e) => {
     setRemarks(e.target.value);
@@ -13,32 +14,41 @@ const Form = () => {
 		setClickedAnimation(true);
 		setTimeout(() => setClickedAnimation(false), 2000);
 	}
+
+	
 	
 	return (
     <form>
       <div className="container">
         <div className="input-container">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" placeholder="Ali bin Abu" />
         </div>
         <div className="input-container">
-          <label htmlFor="contactNumber">Contact Number</label>
-          <input type="text" id="contactNumber" />
+          <label htmlFor="contactNumber">Contact no.</label>
+          <input type="text" id="contactNumber" placeholder="01144598329" />
         </div>
         <div className="input-container">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" />
+          <input type="text" id="email" placeholder="alibinabu@gmail.com" />
         </div>
         <div className="input-container">
           <label htmlFor="gender">Gender</label>
-          <select name="gender" id="gender">
+          <select
+            name="gender"
+            id="gender"
+            className={remarks == "" ? "placeholder" : ""}
+          >
+            <option value="" className="placeholder">
+              Select an option
+            </option>
             <option>Male</option>
             <option>Female</option>
           </select>
         </div>
         <div className="input-container">
           <label htmlFor="age">Age</label>
-          <input type="number" id="age" />
+          <input type="number" id="age" placeholder="21" />
         </div>
         <div className="input-container">
           <label
@@ -53,26 +63,34 @@ const Form = () => {
             id="remarks"
             value={remarks}
             onChange={handleRemarksChange}
+            className={remarks == "" ? "placeholder" : ""}
           >
-            <option value="remark1">Remarks 1</option>
-            <option value="remark2">Remarks 2</option>
+            <option value="" className="placeholder">
+              Select an option
+            </option>
+            <option value="remark1">Option 1</option>
+            <option value="remark2">Option 2</option>
             <option value="others">Others</option>
           </select>
         </div>
         {remarks !== "others" && (
           <div className="input-container">
             <label htmlFor="others">Others</label>
-            <textarea type="text" id="others" disabled/>
+            <textarea type="text" id="others" disabled placeholder="Select other remarks to enter"/>
           </div>
         )}
         {remarks === "others" && (
           <div className="input-container">
             <label htmlFor="others">Others</label>
-            <textarea type="text" id="others" />
+            <textarea type="text" id="others" placeholder="Enter other remarks here"/>
           </div>
         )}
         <div className="button-container">
-          <button className={clickedAnimation ? "form-btn button-bounce" : "form-btn"} type="submit" onClick={bounceAnimation}>
+          <button
+            className={clickedAnimation ? "form-btn button-bounce" : "form-btn"}
+            type="submit"
+            onClick={bounceAnimation}
+          >
             Submit
           </button>
         </div>
